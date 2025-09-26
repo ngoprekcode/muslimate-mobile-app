0c4084386e5c28c2d2ed3e2ee22d83bd36a189f5
Andi Fauzy <andidewantara7@gmail.com>
2025-09-27 00:14:05 +0700

    feat: configure remote_config

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:muslimate_mobile_app/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -28,6 +29,13 @@ Future<void> main() async {
 
       /// Initial Firebase Analytics.
       FirebaseAnalytics.instance;
+
+      /// Initial Firebase Remote Config.
+      final remoteConfig = FirebaseRemoteConfig.instance;
+      await remoteConfig.setConfigSettings(RemoteConfigSettings(
+        fetchTimeout: const Duration(minutes: 1),
+        minimumFetchInterval: const Duration(hours: 1),
+      ));
 
       /// Pass all uncaught errors from the framework to Crashlytics.
       FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
